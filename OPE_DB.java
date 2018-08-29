9b45c2655abcbe08fbca40ae579d6fa54f8506f9
ning shen <shenning1003@gmail.com>
2018-08-29 17:42:39 -0600

    update

--- OPE_DB.java
+++ OPE_DB.java
@@ -19,12 +19,13 @@ public class OPE_DB {
 
 	static final String OPE_user = "";
 	static final String OPE_password = "";
-	
+
 	Query_parser sqlParser;
 	OPE ope;
 	KeyStructure keyFile;
 	private Connection OPE_conn;
 	private Statement OPE_stmt;
+
 	public OPE_DB(DB_connection conn) {
 		this.sqlParser = new Query_parser();
 		keyFile = KeyReader.readKey();
@@ -34,25 +35,26 @@ public class OPE_DB {
 			Class.forName("com.mysql.jdbc.Driver");
 			OPE_conn = DriverManager.getConnection(OPE_DB_URL, OPE_user, OPE_password);
 			OPE_stmt = OPE_conn.createStatement();
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/*
 	 * Create the ope encrypted database by creating each table
 	 */
 	public void EncryptDB() {
 		EncryptSalaryTable();
 	}
-	
+
 	/*
 	 * Return status code, if success return 1, else return 0;
 	 */
-	private int EncryptSalaryTable(){
-		//first if we can find key file;
+	private int EncryptSalaryTable() throws SQLException {
+		PreparedStatement insertStatement = null;
+		// first if we can find key file;
 		TableKey salaryTableKey = keyFile.getSingleTableKeys("ope_salary");
-		if(salaryTableKey == null)
+		if (salaryTableKey == null)
 			return 0;
 		DB_connection db = new DB_connection();
 		ArrayList<Salary> salaries = new ArrayList<Salary>();
@@ -63,30 +65,63 @@ public class OPE_DB {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		if(salaries.size() == 0){
+		if (salaries.size() == 0) {
 			return 0;
-		}
-		else {
+		} else {
 			String sql = "INSERT INTO ope_salary VALUES (?, ?, ?, ?)";
-			for (Salary s : salaries) {
-				try {
-					PreparedStatement insertStatement = this.OPE_conn.prepareStatement("");
+			try {
+				insertStatement = this.OPE_conn.prepareStatement(sql);
+				for (Salary s : salaries) {
+					// emp_no column
 					ColumnKey empIdKey = salaryTableKey.getSingleColumn("emp_no");
-					BigInteger ope_emp_no = ope.OPE_encrypt(BigInteger.valueOf(s.getEmp_no()), empIdKey.getDataKey(), 
+					BigInteger ope_emp_no = ope.OPE_encrypt(BigInteger.valueOf(s.getEmp_no()), empIdKey.getDataKey(),
 							empIdKey.getDomainBit(), empIdKey.getRangeBit());
+					// salary column
 					ColumnKey salaryKey = salaryTableKey.getSingleColumn("salary");
-					BigInteger ope_salary = ope.OPE_encrypt(plaintext, key, domainBit, rangeBit)
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}  
+					BigInteger ope_salary = ope.OPE_encrypt(BigInteger.valueOf(s.getSalary()), salaryKey.getDataKey(),
+							salaryKey.getDomainBit(), salaryKey.getRangeBit());
+					// from_date column
+					ColumnKey fromDateKey = salaryTableKey.getSingleColumn("from_date");
+					BigInteger ope_fromDate = ope.OPE_encrypt(BigInteger.valueOf(Helper.DateToNumber(s.getFromDate())),
+							fromDateKey.getDataKey(), fromDateKey.getDomainBit(), fromDateKey.getRangeBit());
+					// from_to column
+					ColumnKey toDateKey = salaryTableKey.getSingleColumn("to_date");
+					BigInteger ope_toDate = ope.OPE_decrypt(BigInteger.valueOf(Helper.DateToNumber(s.getToDate())),
+							toDateKey.getDataKey(), toDateKey.getDomainBit(), toDateKey.getRangeBit());
+					// create query
+					
+					insertStatement.setString(1, ope_emp_no.toString());
+					insertStatement.setString(2, ope_salary.toString());
+					insertStatement.setString(3, ope_fromDate.toString());
+					insertStatement.setString(4, ope_toDate.toString());
+					insertStatement.executeUpdate();
+					OPE_conn.commit();
+				}
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				if (OPE_conn!= null){					
+					try {
+						System.err.println("Transaction is being rolled back");
+						OPE_conn.rollback();
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}finally {
+				  if(insertStatement != null) {
+					  insertStatement.close();
+				  }
+				  OPE_conn.setAutoCommit(true);
 			}
+
 			return 1;
 		}
-		
+
 	}
-	
-	
+
 	public void InsertFakeTuple() throws IOException {
 		if (keyFile == null)
 			return;
@@ -94,8 +129,8 @@ public class OPE_DB {
 			return;
 		for (TableKey tk : keyFile.tablesKey) {
 			StringBuffer sb = new StringBuffer();
-			for (int index =0; index < tk.get_fkNum(); index++) {
-				sb.append("INSERT INTO " + tk.getTableName()+ " (");
+			for (int index = 0; index < tk.get_fkNum(); index++) {
+				sb.append("INSERT INTO " + tk.getTableName() + " (");
 				for (ColumnKey ck : tk.getAllColumnsKey()) {
 					sb.append(ck.getColumnName() + " ");
 				}
