6b456390c9c9b9467cc5832f0635ccda31dca5e8
ning shen <shenning1003@gmail.com>
2018-09-07 18:11:22 -0600

    update

--- CompletenessValidator.java
+++ CompletenessValidator.java
@@ -25,16 +25,37 @@ public class CompletenessValidator {
 			int domainBit = keys.getSingleTableKeys(tableName).getSingleColumn(attributeName).getDomainBit();
 			int rangeBit = keys.getSingleTableKeys(tableName).getSingleColumn(attributeName).getRangeBit();
 			if (ar.lower.equals(ar.upper)){
-				BigInteger decryptedValue = ope.simple_OPE_decrypt(ar.lower, key, domainBit, rangeBit);
-				BigInteger reEncryptedValue = ope.simple_OPE_encrypt(decryptedValue, key, domainBit, rangeBit);
-				if ()
+				getCorrectIndex(ar.lower, key, domainBit, rangeBit, ar.symbol);
 			}
 			else{
-				if 
+				
 			}
 			
  		}
 		return result;
+	}
+	
+	private BigInteger getCorrectIndex(BigInteger cipher, int key, int domainBit, int rangeBit, String comparator) {
+		//BigInteger result = BigInteger.ZERO;
+		BigInteger decryptedValue = ope.simple_OPE_decrypt(cipher, key, domainBit, rangeBit);
+		BigInteger reEncryptedValue = ope.simple_OPE_encrypt(decryptedValue, key, domainBit, rangeBit);
+		if (reEncryptedValue.compareTo(cipher) == 1) {
+			if(comparator.equals(">")|| comparator.equals(">=")) {
+				return decryptedValue;
+			}
+			else if(comparator.equals("<")|| comparator.equals("<=")) {
+				return decryptedValue.add(BigInteger.ONE);
+			}
+		}
+		else if(reEncryptedValue.compareTo(cipher) == -1) {
+			if (comparator.equals(">")|| comparator.equals(">=")) {
+				return decryptedValue.subtract(BigInteger.ONE);
+			}
+			else if (comparator.equals("<")|| comparator.equals("<=")) {
+				return decryptedValue;
+			}
+		}
+		return decryptedValue;
 	}
 
 }
